3b3702cc2a2b7fd0cbfa133616b3d3ac57030cf8
eoriont <elijahotarr@gmail.com>
2021-04-13 17:18:20 -0700

    small nav and homepage changes

--- assets/js/progressbar.js
+++ assets/js/progressbar.js
@@ -19,18 +19,18 @@ document.addEventListener('scroll', function () {
   /*Apply classes for slide in bar*/
   scrollpos = window.scrollY;
 
-  if (scrollpos > 10) {
-    header.classList.add("bg-white");
-    header.classList.add("shadow");
-    navcontent.classList.remove("bg-gray-100");
-    navcontent.classList.add("bg-white");
-  } else {
-    header.classList.remove("bg-white");
-    header.classList.remove("shadow");
-    navcontent.classList.remove("bg-white");
-    navcontent.classList.add("bg-gray-100");
+  // if (scrollpos > 10) {
+  //   header.classList.add("bg-white");
+  //   header.classList.add("shadow");
+  //   navcontent.classList.remove("bg-gray-100");
+  //   navcontent.classList.add("bg-white");
+  // } else {
+  //   header.classList.remove("bg-white");
+  //   header.classList.remove("shadow");
+  //   navcontent.classList.remove("bg-white");
+  //   navcontent.classList.add("bg-gray-100");
 
-  }
+  // }
 
 });
 
